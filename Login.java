8e9de60d5d3048ad3ac0f8dccc91d28ee7cfc6b2
hariharansj <33515123+hariharansj@users.noreply.github.com>
2017-11-09 16:36:33 +0530

    Patient_login.java

---
+++ Login.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+import net.miginfocom.swing.*;
+/*
+ * Created by JFormDesigner on Thu Nov 09 15:34:07 IST 2017
+ */
+
+
+
+/**
+ * @author Hariharan S
+ */
+public class Success extends JPanel {
+	public Success() {
+		initComponents();
+	}
+
+	private void initComponents() {
+		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+		// Generated using JFormDesigner Evaluation license - Hariharan S
+		label1 = new JLabel();
+
+		//======== this ========
+
+		// JFormDesigner evaluation mark
+		setBorder(new javax.swing.border.CompoundBorder(
+			new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
+				"JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
+				javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
+				java.awt.Color.red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+
+		setLayout(new MigLayout(
+			"hidemode 3",
+			// columns
+			"[fill]" +
+			"[fill]" +
+			"[fill]" +
+			"[fill]" +
+			"[fill]" +
+			"[fill]" +
+			"[fill]" +
+			"[fill]",
+			// rows
+			"[]" +
+			"[]" +
+			"[]"));
+
+		//---- label1 ----
+		label1.setText("Successfully logged-in");
+		add(label1, "cell 3 0 1 2");
+		// JFormDesigner - End of component initialization  //GEN-END:initComponents
+	}
+
+	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+	// Generated using JFormDesigner Evaluation license - Hariharan S
+	private JLabel label1;
+	// JFormDesigner - End of variables declaration  //GEN-END:variables
+}
